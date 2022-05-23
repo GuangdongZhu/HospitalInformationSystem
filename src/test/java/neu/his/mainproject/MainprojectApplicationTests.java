7c9bdf359624a72f24c0d28558a9db0a81eb8517
zhuguangdong <zgd_workemail@163.com>
2022-05-23 21:28:41 +0800

    创建项目阶段

---
+++ src/test/java/neu/his/mainproject/MainprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package neu.his.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
